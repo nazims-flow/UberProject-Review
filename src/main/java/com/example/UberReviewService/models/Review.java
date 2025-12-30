8a2775a919da15244268ae3e0d51f7674d877796
nazims-flow <nazimmazumder68@gmail.com>
2025-12-31 00:00:13 +0530

    Added more columns to the entity

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,13 +1,49 @@
 package com.example.UberReviewService.models;
 
+
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
+@Table(name="bookingreview")
 public class Review {
     
     @Id  // this annotation makes the id property a primary key of our table
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     Long id;
+
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate  // this annotation tells spring that only handle this for the object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // this annotation tells spring that only handle this for the object update
+    Date updatedAt;
+
+
+    
+
 
 
 }
