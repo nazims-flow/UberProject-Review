cc3cfdafb154f4d4f2afbcbb5d5f5c6eeb518b64
nazims-flow <nazimmazumder68@gmail.com>
2025-12-30 23:10:54 +0530

    added first basic model

---
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -0,0 +1,13 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+    
+    @Id  // this annotation makes the id property a primary key of our table
+    Long id;
+
+
+}
